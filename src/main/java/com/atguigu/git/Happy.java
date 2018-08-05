b1ebedcaa831bd8f8b8fb73b74e462165231f302
diguojunren <30565301+diguojunren@users.noreply.github.com>
2018-08-05 14:39:38 +0800

    git23

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -4,7 +4,7 @@ public class Happy {
         public static void main(String[] args) {
 			System.out.println("right-------");
 			System.out.println("left.....");
-			System.out.println("TestGit22");
+			System.out.println("TestGit23");
 
 		}
 }
