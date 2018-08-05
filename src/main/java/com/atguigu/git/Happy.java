1438726b8aa3dd18ffdf434f6cb845d3e0a7f7af
diguojunren <30565301+diguojunren@users.noreply.github.com>
2018-08-05 09:52:09 +0800

    解决了冲突

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -1,14 +1,8 @@
 package com.atguigu.git;
 
 public class Happy {
-<<<<<<< HEAD
         public static void main(String[] args) {
 			System.out.println("right-------");
+			System.out.println("left.....");
 		}
-=======
-	public static void main(String[] args) {
-		System.out.println("left.....");
-	}
-
->>>>>>> branch 'master' of https://github.com/diguojunren/TestGit.git
 }
